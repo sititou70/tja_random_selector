0bf991ad3f0cc4b88662f390a7e43123cf4c0283
Shizukuishi Takuya <b1015208@fun.ac.jp>
2016-04-29 17:18:09 +0900

    UI完成

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ App.on("window-all-closed", function(){
 // Electronの初期化完了後に実行
 App.on("ready", function(){
 	// メイン画面の表示。ウィンドウの幅、高さを指定できる
-	mainWindow = new BrowserWindow({width: 800, height: 500, resizable: false, center: true, autoHideMenuBar: true});
+	mainWindow = new BrowserWindow({width: 600, height: 375, resizable: false, center: true, autoHideMenuBar: true});
 	mainWindow.loadURL("file://" + __dirname + "/index.html");
 	
 	// ウィンドウが閉じられたらアプリも終了
